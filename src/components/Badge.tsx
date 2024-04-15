7e61b75acb400d08e42c6d846862cc970ae2c4bf
Tito Garrido Ogando <titogarrido@gmail.com>
2024-04-15 18:55:12 -0300

    changed branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -47,14 +47,14 @@ export const Badge = (props: Props) => {
       Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://code55.io'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Code55.io</span>
       </a>
     </span>
   );
